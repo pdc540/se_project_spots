c036c29b0d6856fbbe28eb969b59c45fa40c9017
PDC <pdc050400@gmail.com>
2024-09-06 15:03:17 -0400

    Sprint 4 pt 1

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const initialCards [
+
+  let estesparkco = {
+    name: "Estes Park, Colorado",
+    link: "../images/estes.jpg",
+  };
+
+  let barcelona = {
+    name: "Barcelona, Spain",
+    link: "../images/barcelona.jpg",
+  };
+
+  let ibiza = {
+    name: "Ibiza, Spain",
+    link: "../images/ibitha.jpg",
+  };
+
+  let nyc = {
+    name: "New York City, NY",
+    link: "../images/nyc.jpg",
+  };
+
+  let oahu = {
+    name: "Oahu, Hawaii",
+    link: "../images/oahu.jpg",
+  };
+
+  let vegas = {
+    name: "Las Vegas, Nevada",
+    link: "../images/vegas.jpg",
+  };
+
+]
