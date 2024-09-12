4c148d654ccff762409a4757ea3e8e485af1900a
PDC <pdc050400@gmail.com>
2024-09-12 17:56:24 -0400

    Sprint 4 Final submission

--- scripts/index.js
+++ scripts/index.js
@@ -31,13 +31,56 @@ const initialCards = [
 ];
 
 const modal = document.querySelector("#editprofilemodal");
+const formElement = modal.querySelector(".modal__form");
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const modalCloseBtn = document.querySelector(".modal__close-btn");
 
+const profileName = document.querySelector(".profile__name");
+const profileDesc = document.querySelector(".profile__description");
+
+const nameInput = modal.querySelector("#name");
+const descInput = modal.querySelector("#description");
+
+const submitButton = modal.querySelector(".modal__submit-btn");
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameElement = cardElement.querySelector(".card__title");
+  cardNameElement.textContent = data.name;
+  const cardImageElement = cardElement.querySelector(".card__image");
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+
+  return cardElement;
+}
+
 editProfileBtn.addEventListener("click", function () {
   modal.classList.add("modal__opened");
+  nameInput.value = profileName.textContent;
+  descInput.value = profileDesc.textContent;
 });
 
 modalCloseBtn.addEventListener("click", function () {
   modal.classList.remove("modal__opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDesc.textContent = descInput.value;
+  modal.classList.remove("modal__opened");
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+for (i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
